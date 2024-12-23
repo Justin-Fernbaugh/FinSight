a285d2deffbf0a7a6799d1a1e7f481ede816be86
Justin <fernbaughj@gmail.com>
2024-12-23 15:07:47 -0800

    Pass token to handler

--- main.go
+++ main.go
@@ -1,28 +1,32 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"github.com/Justin-Fernbaugh/FinSight/handler"
 )
 
 const (
 	serviceName = "FinSight"
 )
 
-var command = &cobra.Command {
-	Use: serviceName,
-	Run: run,
-}
+var (
+	command = &cobra.Command {
+		Use: serviceName,
+		Run: run,
+	}
+	clientID string
+	token string
+	databaseName string
+)
 
 func init() {
-		// Add flags
-		command.Flags().String("ynab-client-id", "", "The YNAB application client ID (required)")
-		command.Flags().String("ynab-token", "", "The YNAB client secret (required)")
-		command.Flags().String("database-name", "", "The GCP Firestore database name (required)")
+		command.Flags().StringVar(&clientID, "ynab-client-id", "", "The YNAB application client ID (required)")
+		command.Flags().StringVar(&token, "ynab-token", "", "The YNAB client secret (required)")
+		command.Flags().StringVar(&databaseName, "database-name", "", "The GCP Firestore database name (required)")
 
 		// Mark the flags as required
 		for _, flag := range []string{"ynab-client-id", "ynab-token", "database-name"} {
@@ -43,10 +47,11 @@ func init() {
 }
 
 func run(cmd *cobra.Command, args []string) {
+	log.Println("Starting FinSight ...")
 
-	fmt.Println("Hello World")
-
-	return
+	if err := handler.NewHandler(token); err != nil {
+		log.Fatalf("Error creating handler: %v", err)
+	}
 }
 
 func main() {
